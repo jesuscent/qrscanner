aab425b442820b3ec9c7bed21edffebb46664a25
jcenteno <jcenteno@cecgroup.mx>
2021-05-25 17:55:54 -0500

    card adaptables

--- App.js
+++ App.js
@@ -1,17 +1,10 @@
-
 import React, {useState} from 'react';
 import QRCodeScanner from 'react-native-qrcode-scanner'
-import { Text,Image, Linking,StyleSheet,Separator, TouchableOpacity, Alert, TouchableHighlight , View, Modal, Button ,Pressable} from 'react-native';
+import { Text,Image, ImageBackground,Linking,StyleSheet,Separator, TouchableOpacity, Alert, TouchableHighlight , View, Modal, Button ,Pressable, ScrollView} from 'react-native';
 
 import AwesomeAlert from 'react-native-awesome-alerts';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { Colors,  DebugInstructions,  Header,  LearnMoreLinks,  ReloadInstructions,} from 'react-native/Libraries/NewAppScreen';
 
 
 function App() {
@@ -40,14 +33,15 @@ function App() {
        )
       .catch(console.log)
   }
-
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(prevCount => prevCount + 1);
   return (
     <View>
       <QRCodeScanner
         containerStyle={{backgroundColor: 'white'}}
         onRead={alerta}
-        reactivate={true}
-        reactivateTimeout={3000}
+       reactivate={true}
+       //reactivateTimeout={6000}
         permissionDialogMessage="¿Puedo usar tu camara?"
         showMarker={true}
         markerStyle={{borderColor: 'green', borderRadius: 10}}
@@ -76,72 +70,60 @@ function App() {
           setModalVisible(true);
         }}
       />
-
       <Modal animationType="slide" transparent={false} visible={modalVisible}>
-        <View >
-          <Text> {json.nombre}</Text>
-          <TouchableOpacity style={styles.viewcard1}>
-          <View style={styles.viewcardcontent}> 
-          <Text style={styles.texto}>
-              Entrada dia 1: {"\n"}
-             Fbx dia 1 {json.evento1} {"\n"} {"\n"}            
-              nombre invitado: {"\n"}
-             Jesús Centeno {json.evento1}  {"\n"}    {"\n"}        
-              Acompañante: {"\n"}
-              carlos santos {json.evento1} 
-              </Text>
-          </View>
-          <View style={styles.viewcardcontent}>
-          <Image
-        style={styles.Logo}
-        source={{
-          uri: 'https://reactnative.dev/img/tiny_logo.png',
-        }}
-      />
-              </View>
+     
+        <ScrollView>
+          <Text style={styles.titulo}>Entrada de evento {count}</Text>          
+          <TouchableOpacity style={styles.viewcard1} onPress={onPress} >          
+                <View style={styles.viewcardcontent}> 
+                <Text style={styles.tituloCard}>Invitado:</Text> 
+                <Text style={styles.texto}> Jesús guadalupe Centeno centeno {json.evento1}  </Text> 
+                <Text style={styles.tituloCard}>Acompañante:</Text> 
+                <Text style={styles.texto}> Jesús guadalupe Centeno centeno {json.evento1}  </Text> 
+                <Text style={styles.texto}>{"\n"}25 de junio 2021</Text>                   
+                </View>
+                <View style={styles.viewcardcontent2}> 
+                <Text style={styles.evento}>DINNER & DRINKS</Text>
+                <Image style={styles.Logo}
+              source={{
+                uri:('https://fbx40.azurewebsites.net/static/media/ftlogo.9e8078a5.png')
+              }}
+            />
+             </View> 
           </TouchableOpacity>
-          <TouchableOpacity style={styles.viewcard1}>
-          
-          <Text style={styles.texto}>
-              Entrada dia 1: {"\n"}{"\n"}
-             Fbx dia 1 {json.evento1} 
-              </Text>
-            <Text style={styles.texto}>
-              Nombre invitado:{"\n"}
-              Jesus centeno {json.evento1} 
-              </Text>
-              <Text style={styles.texto}>
-              Nombre acompañante: {"\n"}
-               guadalupe Centeno {json.evento1} 
-              </Text>              
+          <TouchableOpacity style={styles.viewcard2} >          
+                <View style={styles.viewcardcontent}> 
+                <Text style={styles.tituloCard}>Invitado:</Text> 
+                <Text style={styles.texto}>Jesús guadalupe Centeno centeno {json.evento1}  </Text> 
+                <Text style={styles.tituloCard}>Acompañante:</Text> 
+                <Text style={styles.texto}> Jesús guadalupe Centeno centeno {json.evento1}  </Text> 
+                <Text style={styles.texto}>{"\n"}26 de junio 2021</Text>                   
+                </View>
+                <View style={styles.viewcardcontent2}> 
+                <Text style={styles.evento}>BRUNCH</Text>
+                <Image style={styles.Logo}
+              source={{
+                uri:('https://fbx40.azurewebsites.net/static/media/ftlogo.9e8078a5.png')
+              }}
+            />
+             </View> 
           </TouchableOpacity>
-          <TouchableOpacity style={styles.viewcard2}>
-            
-          <Text style={styles.texto}>
-              Entrada dia 2:
-             Fbx dia 1 {json.evento1} 
-              </Text>
-            <Text style={styles.texto}>
-              Nombre invitado:
-              Jesus centeno {json.evento1} 
-              </Text>
-              <Text style={styles.texto}>
-              Nombre acompañante: guadalupe Centeno {json.evento1} 
-              </Text>           
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.viewcard3}>           
-          <Text style={styles.texto}>
-              Entrada dia 3:
-             Fbx dia 1 {json.evento1} 
-              </Text>
-            <Text style={styles.texto}>
-              Nombre invitado:
-              Jesus centeno {json.evento1} 
-              </Text>
-              <Text style={styles.texto}>
-              Nombre acompañante: guadalupe Centeno {json.evento1} 
-              </Text>           
+          <TouchableOpacity style={styles.viewcard3} >          
+                <View style={styles.viewcardcontent}> 
+                <Text style={styles.tituloCard}>Invitado:</Text> 
+                <Text style={styles.texto}> Jesús guadalupe Centeno centeno {json.evento1}  </Text> 
+                <Text style={styles.tituloCard}>Acompañante:</Text> 
+                <Text style={styles.texto}> Jesús guadalupe Centeno centeno {json.evento1}  </Text> 
+                <Text style={styles.texto}>{"\n"}27 de junio 2021</Text>                   
+                </View>
+                <View style={styles.viewcardcontent2}> 
+                <Text style={styles.evento}>ZAMNA</Text>
+                <Image style={styles.Logo}
+              source={{
+                uri:('https://fbx40.azurewebsites.net/static/media/ftlogo.9e8078a5.png')
+              }}
+            />
+             </View> 
           </TouchableOpacity>
           
           <TouchableOpacity style={styles.btn} onPress={() => setModalVisible(!modalVisible)} >
@@ -149,7 +131,7 @@ function App() {
           ESCANEAR NUEVO INVITADO
               </Text> 
           </TouchableOpacity>
-        </View>
+        </ScrollView>
       </Modal>
     </View>
   );
@@ -157,69 +139,110 @@ function App() {
  }
 
  const styles = StyleSheet.create({
+   titulo:{
+     textAlign:'center',
+     fontSize:30,
+     fontWeight:'bold',
+     backgroundColor: '#0277bd',
+     paddingBottom:2,
+     marginBottom:4,
+      color:'#FFFFFF', 
+   },
+   tituloCard:{
+    textAlign:'center',
+    fontWeight:'bold',
+    color:'#FFFFFF', 
+  },
   viewcardcontent:{
-    flexDirection: "row",
-    flexWrap: "wrap",
+    flexDirection: "column",
     shadowColor: '#a5e1ad',
-    shadowOffset: { width: 2, height: 1 },
-    shadowRadius: 1,
-    shadowOpacity: 0.26,
-    elevation: 1,
+    width:'55%',
+    height:'auto',
     padding: 10,
-    margin:5,
+    marginLeft:5,
+    alignItems:'stretch',
+    textAlign:'center',
+   },
+   viewcardcontent2:{
+    flexDirection: "column",
+    padding: 5,
+    marginLeft:6,    
+    width:'38%',
+    textAlign:'center', 
+    alignItems:'center',
    },
  viewcard1:{
   flexDirection: "row",
   flexWrap: "wrap",
   shadowColor: '#a5e1ad',
-  shadowOffset: { width: 6, height: 5 },
-  shadowRadius: 8,
+  shadowOffset: { width: 5, height: 6 },
+  shadowRadius: 4,
   shadowOpacity: 0.26,
   elevation: 8,
-  backgroundColor: '#511281',
-  padding: 20,
+  backgroundColor: '#CB910C',  
   borderRadius: 15,
-  margin:12,
+  margin:5,  
  },
  viewcard2:{
-  shadowColor: '#511281',
-  shadowOffset: { width: 6, height: 5 },
-  shadowRadius: 8,
+  flexDirection: "row",
+  flexWrap: "wrap",
+  shadowColor: '#a5e1ad',
+  shadowOffset: { width: 5, height: 6 },
+  shadowRadius: 4,
   shadowOpacity: 0.26,
   elevation: 8,
-  backgroundColor: '#4ca1a3',
-  padding: 20,
+  backgroundColor: '#9c4dcc',  
   borderRadius: 15,
-  margin:12,
+  margin:5,  
  },
  viewcard3:{
-  shadowColor: '#511281',
-  shadowOffset: { width: 6, height: 5 },
-  shadowRadius: 8,
+  flexDirection: "row",
+  flexWrap: "wrap",
+  shadowColor: '#a5e1ad',
+  shadowOffset: { width: 5, height: 6 },
+  shadowRadius: 4,
   shadowOpacity: 0.26,
   elevation: 8,
-  backgroundColor: '#a5e1ad',
-  padding: 20,
+  backgroundColor: '#38006b',  
   borderRadius: 15,
-  margin:12,
+  margin:5,  
  },
  texto:{  
-  flexDirection: "column",
+  flexDirection: "row",
   padding: 1,
-  color:'#FFFFFF',  
+  color:'#FFFFFF', 
+  textAlign:'center',
  },
- btn:{
-  
-   justifyContent: 'center',
-    marginHorizontal: 16,
-  backgroundColor: '#511281',
+ evento:{  
+  fontSize:14,
+  fontWeight:'bold',
+  color:'#FFFFFF', 
+  textAlign:'center',
+ },
+ btn:{  
+  justifyContent: 'center',
+  marginHorizontal: 50,
+  margin:25,
+  backgroundColor: '#0277bd',
+  shadowColor: '#a5e1ad',
+  shadowOffset: { width: 5, height: 6 },
+  shadowRadius: 4,
+  shadowOpacity: 0.26,
+  elevation: 8,
+  borderRadius:50,
+  width: '70%',
+  height: 50,
+  textAlign:'center',
  },
  Logo:{
-   marginBottom:2,
-  width: 110,
-  height: 120,
+  marginTop:20,
+  width: 120,
+  height: 100,
+  resizeMode: "cover",
+  alignContent:'center',
+  alignItems:'center'
+ },
  
- }
 });
 
 export default App;
